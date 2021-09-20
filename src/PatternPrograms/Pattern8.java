1c6ab0d27924d1136285f54d9e626c0475d167f5
Ponraj N <ponraj_n@photonint.com>
2021-09-20 22:44:31 +0530

    text

--- src/PatternPrograms/Pattern8.java
+++ src/PatternPrograms/Pattern8.java
@@ -4,6 +4,19 @@ public class Pattern8 {
 
 	public static void main(String[] args) {
 		
+		for(int row=4;row>=1;row-- ) {
+			for (int col = 1; col <=row;col++) {
+				System.out.print(col + " ");
+			}
+			System.out.println(" ");
+		}
+		
+		for(int row=1;row<=4;row++ ) {
+			for (int col = 1; col <=row;col++) {
+				System.out.print(col + " ");
+			}
+			System.out.println(" ");
+		}
 
 	}
 
